98d6f6e0f5434143b7bf6a431a9f4be5ce3d7f2e
Kotmin <70173732+Kotmin@users.noreply.github.com>
2024-03-13 19:01:00 +0100

    quite nice view of pentagon, not working search func

--- CoreDataRecap/View/MapsView.swift
+++ CoreDataRecap/View/MapsView.swift
@@ -9,12 +9,42 @@ import SwiftUI
 import MapKit
 
 struct MapsView: View {
-    
+    // Map
     @State private var cameraPosition: MapCameraPosition = .region(.userRegion)
+    @State private var mapSelection: MKMapItem?
+    // 16:15
+    @State private var viewingRegion: MKCoordinateRegion?
+    
+    // Searching
+    @State private var searchResults: [MKMapItem] = []
+    
+    // Route navigation
+    
+    @State private var routeDisplaying: Bool = false
+    @State private var route: MKRoute?
     
     var body: some View {
-        Map(position: $cameraPosition){
-            Marker("My location",systemImage: "paperplane", coordinate: .userLocation)
+        Map(position: $cameraPosition,selection: $mapSelection){
+//            Marker("My location",systemImage: "paperplane", coordinate: .userLocation)
+            
+            Annotation("Pentagon Pollub", coordinate: .userLocation){
+                
+                ZStack{
+                    Image(systemName: "cat")
+                        .font(.title3)
+                        .foregroundColor(Color.brown)
+                    
+                    Image(systemName: "square")
+                        .font(.largeTitle)
+                }
+                
+//                ForEach(searchResults, id: \.self){
+//                    mapItem in let placemark = mapItem.placemark
+//                    Marker(placemark.name ?? "Place",coordinate: placemark.coordinate).tint(.blue)
+//                }
+                
+                
+            }
         }
         .mapControls {
                     MapPitchToggle()
@@ -22,6 +52,33 @@ struct MapsView: View {
                     MapUserLocationButton()
                 }
     }
+    
+    func searchPlaces() async {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "Cafe Heca Lublin"
+        request.region = .userRegion // viewingRegion ?? .userRegion
+        
+        let results = try? await MKLocalSearch(request: request).start()
+        searchResults = results?.mapItems ?? []
+    }
+    
+    func fetchRoute() {
+        if let mapSelection {
+            let request = MKDirections.Request()
+            request.source = .init(placemark: .init(coordinate: .userLocation))
+            request.destination = mapSelection
+            
+            Task {
+                let result = try? await MKDirections(request: request).calculate()
+                route = result?.routes.first
+                
+                withAnimation(.snappy) {
+                    routeDisplaying = true
+                }
+            }
+        }
+    }
+    
 }
 
 extension CLLocationCoordinate2D {
